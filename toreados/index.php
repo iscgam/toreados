73249ba285aff755fe8d22237dc3741c173e5aef
iscgam <isc.gam@gmail.com>
2025-03-12 12:11:41 -0600

    subiendo el index

---
+++ toreados/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Burros Toreados - Los burros que no aburren</title>
+  <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+  <div style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
+    <video id="videot" autoplay muted>
+      <source src="img/video.mp4" type="video/mp4">
+    </video>
+    <img src="img/menu-1.png" alt="burros trabajando" style="width: 100%"/>
+    <img src="img/menu-2.png" alt="burros trabajando" style="width: 100%"/>
+  </div>
+</body>
+</html>
