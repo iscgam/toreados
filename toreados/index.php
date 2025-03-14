d2f31292e7feccb2644d0af8d594e1d21a592809
iscgam <isc.gam@gmail.com>
2025-03-14 15:38:35 -0600

    Update index.php
    Se agregó el video de wix

--- toreados/index.php
+++ toreados/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Burros Toreados - Los burros que no aburren</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Burros Toreados - Los burros que no aburren</title>
   <link rel="stylesheet" type="text/css" href="estilos.css">
 </head>
 <body>
   <div style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
     <video id="videot" autoplay muted>
-      <source src="img/video.mp4" type="video/mp4">
+      <source src="https://video.wixstatic.com/video/ff70d6_62a3afb971924b499d5a17e429bca1c4/480p/mp4/file.mp4" type="video/mp4">
     </video>
     <img src="img/menu-1.png" alt="burros trabajando" style="width: 100%"/>
     <img src="img/menu-2.png" alt="burros trabajando" style="width: 100%"/>
